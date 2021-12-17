c4b1415beb6f5d3ba5207bfd9f8684ba2ed34d15
Artemowkin <artyom.groshev2017@gmail.com>
2021-12-18 01:42:50 +0700

    Added category serializer

---
+++ categories/tests/test_serializers.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+
+from ..models import Category
+from ..serializers import CategorySerializer
+
+
+class CategorySerializerTestCase(TestCase):
+
+	def setUp(self):
+		self.category = Category.objects.create(title='some category')
+		self.category_data = {
+			'pk': str(self.category.pk),
+			'title': self.category.title,
+		}
+
+	def test_serialized_category(self):
+		serialized_category = CategorySerializer(self.category).data
+		self.assertEqual(serialized_category, self.category_data)
+
+	def test_is_data_valid(self):
+		serializer = CategorySerializer(data=self.category_data)
+		self.assertTrue(serializer.is_valid())
