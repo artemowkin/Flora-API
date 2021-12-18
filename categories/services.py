ce16a22a2ae37d0e8d2892b32e858bee63b5ef85
Artemowkin <artyom.groshev2017@gmail.com>
2021-12-18 17:29:33 +0700

    Added CategoryCRUDFacade

--- categories/services.py
+++ categories/services.py
@@ -2,8 +2,12 @@ from uuid import UUID
 from typing import Union
 
 from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
 
 from .models import Category
+
+
+User = get_user_model()
 
 
 class GetCategoriesService:
@@ -16,3 +20,15 @@ class GetCategoriesService:
 		"""Return a concrete category using pk field. 404 if not found"""
 		category = get_object_or_404(self._model, pk=category_pk)
 		return category
+
+
+class CategoryCRUDFacade:
+	"""Facade with CRUD functionality for categories"""
+
+	def __init__(self, user: User) -> None:
+		self._user = user
+		self._get_categories_service = GetCategoriesService()
+
+	def get_concrete(self, category_pk: Union[UUID,str]) -> Category:
+		"""Return a concrete category"""
+		return self._get_categories_service.get_concrete(category_pk)
