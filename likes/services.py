7ce35cae45d522258711a4302d58a955ecfd283d
Artemowkin <artyom.groshev2017@gmail.com>
2021-12-23 12:43:52 +0700

    Added like_project service

--- likes/services.py
+++ likes/services.py
@@ -1,6 +1,25 @@
+from uuid import UUID
+from typing import Union
+
+from django.shortcuts import get_object_or_404
 from django.db.models import QuerySet
+from django.contrib.auth import get_user_model
+
+from projects.models import Project
+from .models import Like
+
+
+User = get_user_model()
 
 
 def get_likes_count(likes: QuerySet) -> int:
 	"""Return project likes count"""
 	return likes.count()
+
+
+def like_project(project_pk: Union[UUID,str], user: User) -> bool:
+	"""Like project by user if not liked"""
+	project = get_object_or_404(Project, pk=project_pk)
+	like, created = Like.objects.get_or_create(project=project, user=user)
+	if not created: return False
+	return True
--- likes/tests/test_services.py
+++ likes/tests/test_services.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-from ..services import get_likes_count
+from ..services import get_likes_count, like_project
 from ..models import Like
 from projects.models import Project
 from categories.models import Category
@@ -25,3 +25,27 @@ class GetLikesCountServiceTestCase(TestCase):
 
 	def test_get_projects_like_count(self):
 		self.assertEqual(get_likes_count(self.project.likes.all()), 1)
+
+
+class LikeProjectServiceTestCase(TestCase):
+
+	def setUp(self):
+		self.user = User.objects.create_superuser(
+			username='testuser', password='testpass'
+		)
+		self.category = Category.objects.create(title='some category')
+		self.project = Project.objects.create(
+			title='some project', description='some description',
+			category=self.category, user=self.user
+		)
+
+	def test_like_unliked_project(self):
+		liked = like_project(self.project.pk, self.user)
+		self.assertEqual(self.project.likes.count(), 1)
+		self.assertTrue(liked)
+
+	def test_like_already_liked_project(self):
+		like_project(self.project.pk, self.user)
+		liked = like_project(self.project.pk, self.user)
+		self.assertFalse(liked)
+		self.assertEqual(self.project.likes.count(), 1)
