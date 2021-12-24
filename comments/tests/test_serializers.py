0fa39196e19bf2082bf01baba9fe11c6e12cd462
Artemowkin <artyom.groshev2017@gmail.com>
2021-12-24 23:31:06 +0700

    Documented all project comments endpoint

--- comments/tests/test_serializers.py
+++ comments/tests/test_serializers.py
@@ -32,3 +32,27 @@ class CommentSerializerTestCase(TestCase):
 		self.assertEqual(serialized_comment['user'], self.user.username)
 		self.assertEqual(serialized_comment['text'], self.comment.text)
 		self.assertIn('pub_datetime', serialized_comment)
+
+	def test_reply_on_field(self):
+		new_comment = Comment.objects.create(
+			project=self.project, user=self.user, text='second comment',
+			reply_on=self.comment
+		)
+		serialized_comment = CommentSerializer(new_comment).data
+		self.assertEqual(serialized_comment['reply_on'], self.comment.pk)
+
+	def test_is_data_valid_without_reply_on(self):
+		data = {
+			'project': str(self.project.pk), 'user': self.user.id,
+			'text': 'some comment'
+		}
+		serializer = CommentSerializer(data=data)
+		self.assertTrue(serializer.is_valid())
+
+	def test_is_data_valid_with_reply_on(self):
+		data = {
+			'project': str(self.project.pk), 'user': self.user.id,
+			'text': 'some comment', 'reply_on': str(self.comment.pk)
+		}
+		serializer = CommentSerializer(data=data)
+		self.assertTrue(serializer.is_valid())
