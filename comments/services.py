afae82eee65fffc7aacedf8cd4f3259b71086233
Artemowkin <artyom.groshev2017@gmail.com>
2021-12-25 17:24:14 +0700

    Added create project comment service

--- comments/services.py
+++ comments/services.py
@@ -1,13 +1,52 @@
-from typing import Union
+from typing import Union, Optional
 from uuid import UUID
 
 from django.db.models import QuerySet
 from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
+from django.core.exceptions import PermissionDenied
 
 from projects.models import Project
+from .models import Comment
+
+
+User = get_user_model()
 
 
 def get_project_comments(project_pk: Union[UUID,str]) -> QuerySet:
 	"""Return comments on project"""
 	project = get_object_or_404(Project, pk=project_pk)
 	return project.comments.all()
+
+
+class CreateCommentService:
+	"""Service to create a new project comment"""
+
+	def __init__(self, user: User) -> None:
+		if not user.is_authenticated: raise PermissionDenied
+		self._user = user
+
+	def create(self, project_pk: Union[UUID,str], text: str,
+			reply_on_pk: Optional[UUID] = None) -> Comment:
+		"""Create a new project comment"""
+		reply_on_comment = self._get_reply_on_comment(reply_on_pk)
+		project = get_object_or_404(Project, pk=project_pk)
+		self._check_reply_on_comment_project(reply_on_comment, project)
+		return Comment.objects.create(
+			reply_on=reply_on_comment, project=project, user=self._user,
+			text=text
+		)
+
+	def _get_reply_on_comment(self,
+			comment_pk: Optional[UUID]) -> Optional[Comment]:
+		"""Get reply on comment by pk or None if comment_pk is None"""
+		if comment_pk:
+			return get_object_or_404(Comment, pk=comment_pk)
+		else:
+			return None
+
+	def _check_reply_on_comment_project(self, comment: Optional[UUID],
+			project: Project) -> None:
+		"""Check is reply on comment on the same project as creating comment"""
+		if comment and not comment.project == project:
+			raise ValueError
--- comments/tests/test_services.py
+++ comments/tests/test_services.py
@@ -1,10 +1,12 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.core.exceptions import PermissionDenied
+from django.contrib.auth.models import AnonymousUser
 
 from categories.models import Category
 from projects.models import Project
 from ..models import Comment
-from ..services import get_project_comments
+from ..services import get_project_comments, CreateCommentService
 
 
 User = get_user_model()
@@ -37,3 +39,59 @@ class GetProjectCommentsServiceTestCase(TestCase):
 		)
 		comments = get_project_comments(new_project.pk)
 		self.assertEqual(comments.count(), 0)
+
+
+class CreateProjectCommentServiceTestCase(TestCase):
+
+	def setUp(self):
+		self.user = User.objects.create_superuser(
+			username='testuser', password='testpass'
+		)
+		self.category = Category.objects.create(title='Some category')
+		self.project = Project.objects.create(
+			title='Some project', description='Some description',
+			category=self.category, user=self.user
+		)
+		self.create_comment_service = CreateCommentService(self.user)
+
+	def test_create_without_reply_on(self):
+		comment = self.create_comment_service.create(
+			project_pk=self.project.pk, text='some comment'
+		)
+		self.assertIsNone(comment.reply_on)
+		self.assertEqual(comment.project, self.project)
+		self.assertEqual(comment.user, self.user)
+		self.assertEqual(comment.text, 'some comment')
+		self.assertEqual(self.project.comments.count(), 1)
+
+	def test_create_with_reply_on(self):
+		first_comment = self.create_comment_service.create(
+			project_pk=self.project.pk, text='some comment'
+		)
+		second_comment = self.create_comment_service.create(
+			project_pk=self.project.pk, text='second comment',
+			reply_on_pk=first_comment.pk
+		)
+		self.assertEqual(second_comment.reply_on, first_comment)
+		self.assertEqual(second_comment.project, self.project)
+		self.assertEqual(second_comment.user, self.user)
+		self.assertEqual(second_comment.text, 'second comment')
+		self.assertEqual(self.project.comments.count(), 2)
+
+	def test_create_with_reply_on_another_project_comment(self):
+		new_project = Project.objects.create(
+			title='New project', description='Some description',
+			category=self.category, user=self.user
+		)
+		first_comment = self.create_comment_service.create(
+			project_pk=new_project.pk, text='some comment'
+		)
+		with self.assertRaises(ValueError):
+			second_comment = self.create_comment_service.create(
+				project_pk=self.project.pk, text='second comment',
+				reply_on_pk=first_comment.pk
+			)
+
+	def test_create_with_not_authenticated_user(self):
+		with self.assertRaises(PermissionDenied):
+			CreateCommentService(AnonymousUser())
